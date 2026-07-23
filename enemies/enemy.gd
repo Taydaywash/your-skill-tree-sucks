497727ecdae5cb671aaefdb6a252e140bd5e3e45
Kellen-N <kellenswang3@gmail.com>
2026-07-22 22:33:27 -0500

    Simple Health System

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,13 +1,28 @@
 extends CharacterBody2D
 
 @export var speed: float = 100
+@export var damage_amount: int = 20
+@onready var health: Health = $Health
+@onready var health_bar: ProgressBar = $Health/HealthBar
+
 var player: Player = null
 
 func _ready() -> void:
 	player = get_parent().get_node("Player")
+	health_bar.max_value = health.max_health
+	health_bar.value = health.current_health
+	
+	health.health_changed.connect(on_health_changed)
+	health.death.connect(on_death)
 	
 func _physics_process(_delta):
 	if player: 
 		var direction: Vector2 = (player.global_position - global_position).normalized()
 		velocity = direction * speed
 		move_and_slide()
+
+func on_health_changed(new_health: int) -> void:
+	health_bar.value = new_health
+
+func on_death() -> void:
+	call_deferred("queue_free")
--- player/player_movement.gd
+++ player/player_movement.gd
@@ -3,9 +3,13 @@ class_name Player
 
 @export var move_speed : int
 @export var sprite: AnimatedSprite2D
+@onready var health: Health = $Health
+@onready var health_bar: ProgressBar = $Health/HealthBar
 
 func _ready() -> void:
-	pass
+	health.health_changed.connect(on_health_changed)
+	health.death.connect(on_death)
+
 func _physics_process(_delta: float) -> void:
 	var input_dir = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = input_dir.normalized() * move_speed
@@ -21,6 +25,12 @@ func _process(_delta: float) -> void:
 	else:
 		sprite.play("default")
 
-func _on_player_hitbox_area_entered(_area):
-	#get_tree().reload_current_scene.call_deferred()
-	pass
+func on_health_changed(new_health: int) -> void:
+	health_bar.value = new_health
+
+func _on_player_hitbox_area_entered(area):
+	var damage = area.get_parent().damage_amount
+	health.take_damage(damage)
+
+func on_death() -> void:
+	get_tree().call_deferred("reload_current_scene")
