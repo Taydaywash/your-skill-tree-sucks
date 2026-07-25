f5f7a14e5892e2531b3939584749a46dc7d3f8c8
Taydaywash <tayvenwashington1@gmail.com>
2026-07-25 00:32:32 -0500

    Added musica

--- title.gd
+++ title.gd
@@ -12,11 +12,11 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_packed(main)
 
 func _on_settings_pressed() -> void:
-	audio_controller.play_sound(positive,0.7,1)
+	audio_controller.play_sound(positive,0.7,0.9)
 	settings_screen.visible = true
 
 func _on_settings_back_pressed() -> void:
-	audio_controller.play_sound(negative)
+	audio_controller.play_sound(negative,1,1.3)
 	settings_screen.visible = false
 
 func _on_quit_pressed() -> void:
