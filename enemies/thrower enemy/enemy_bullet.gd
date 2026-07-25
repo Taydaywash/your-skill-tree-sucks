90aec55032a669a24a650300d5ca44a2a970f039
Taydaywash <tayvenwashington1@gmail.com>
2026-07-25 15:05:26 -0500

    added projectile killer when leveling down

--- enemies/thrower enemy/enemy_bullet.gd
+++ enemies/thrower enemy/enemy_bullet.gd
@@ -3,10 +3,20 @@ class_name EnemyBullet
 
 @export var speed : int
 
-func _physics_process(_delta: float) -> void:
+func _ready() -> void:
+	await get_tree().create_timer(10).timeout
+	queue_free()
+
+func _physics_process(delta: float) -> void:
+	rotate(delta)
 	move_and_slide()
 
 func _on_damage_area_body_entered(_body: Node2D) -> void:
 	velocity = Vector2.ZERO
 	await get_tree().create_timer(0.1).timeout
 	queue_free()
+
+
+func _on_damage_area_area_entered(area: Area2D) -> void:
+	if area.get_collision_layer_value(6):
+		queue_free()
